7d68d41f3df199bc7cdcb4cd134e3cd7d0d7befe
parsa <parsafarashah@gmail.com>
2025-11-11 11:46:47 +0330

    feat: add sign up click

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 let $sigInBtn = document.querySelector("#sigInBtn");
+let $signUpBtn = document.querySelector("#signUpBtn");
 let $signUpWrapper = document.querySelector("#signUpWrapper");
 let $signInWrapper = document.querySelector("#signInWrapper");
 
@@ -6,4 +7,11 @@ $sigInBtn.addEventListener("click", () => {
   $signUpWrapper.classList.add("left-[130%]");
   $signInWrapper.classList.remove("-left-full");
   $signInWrapper.classList.add("left-1/2");
+});
+
+$signUpBtn.addEventListener("click", () => {
+  $signUpWrapper.classList.remove("left-[130%]");
+  $signUpWrapper.classList.add("left-1/2");
+  $signInWrapper.classList.remove("left-1/2");
+  $signInWrapper.classList.add("-left-full");
 });
